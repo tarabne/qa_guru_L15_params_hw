5b8607c33b49692c1a0b7bb0e3f21c2666bc2af2
Nikita Tarabukhin <tarabne@gmail.com>
2024-02-20 23:53:46 +0300

    fix

--- src/test/java/ru/tarabne/tests/TestBase.java
+++ src/test/java/ru/tarabne/tests/TestBase.java
@@ -17,8 +17,8 @@ public class TestBase {
         Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+//        Configuration.browser = System.getProperty("browser", "chrome");
+//        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
         Configuration.remote = System.getProperty("browserRemoteUrl");
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
